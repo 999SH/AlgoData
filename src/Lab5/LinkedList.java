a2757645efae66bec62639346c29e56d62935f6d
999SH <94057132+999SH@users.noreply.github.com>
2022-09-15 18:49:07 +0200

    Lab 5 task 1 done

--- src/Lab5/LinkedList.java
+++ src/Lab5/LinkedList.java
@@ -1,25 +1,27 @@
 package Lab5;
+
 public class LinkedList {
     Node head;
-    public void addNode(int value){
+
+    public void addNode(int value) {
         Node node = new Node();
         node.value = value;
         node.pointer = null;
 
-        if (head == null){
+        if (head == null) {
             head = node;
-        }
-        else {
+        } else {
             Node travel = head;
-            while (travel.pointer != null){
+            while (travel.pointer != null) {
                 travel = travel.pointer;
             }
             travel.pointer = node;
         }
     }
-    public void printList(){
+
+    public void printList() {
         Node node = head;
-        while(node.pointer != null){
+        while (node.pointer != null) {
             System.out.println(node.value);
             node = node.pointer;
         }
@@ -27,18 +29,58 @@ public class LinkedList {
 
     }
 
-    public int getHead(){
-        return head.value;
+    public Node getHead() {
+        return head;
     }
-    public int getValue(int index){
+
+    public Node getTail() {
+        Node travel = head;
+        while (travel.pointer != null) {
+            travel = travel.pointer;
+        }
+        return travel;
+    }
+
+    public int getValue(int index) {
         Node node = head;
         int i = 0;
-        while(node.pointer != null && i++ < index){
+        while (node.pointer != null && i++ < index) {
             node = node.pointer;
-            if (node.pointer == null && i != index){
+            if (node.pointer == null && i != index) {
                 System.out.println("Index is empty");
             }
         }
         return node.value;
+    }
+
+    public int getValueNode(Node n){
+        return n.value;
+    }
+
+
+    public void addList(LinkedList list, LinkedList append) {
+        Node travel = null;
+        Node appendednode = null;
+        travel = list.getTail();
+        appendednode = append.getHead();
+
+        travel.pointer = appendednode;
+        while (appendednode.pointer != null) {
+            appendednode = appendednode.pointer;
+        }
+
+    }
+
+    public void addNodeWithPointer(Node input){
+            if (head == null) {
+                head = input;
+            }
+            else {
+                Node travel = head;
+                while (travel.pointer != null) {
+                    travel = travel.pointer;
+                }
+                travel.pointer = input;
+            }
     }
 }
--- src/Lab5/Main.java
+++ src/Lab5/Main.java
@@ -4,26 +4,31 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         int[] nArray = new int[6];
-        int ok = 1000;
+        int ok = 500;
         for (int l = 0; l < nArray.length; l++) {
             nArray[l] = ok;
             ok = ok * 2;
         }
         System.out.println(Arrays.toString(nArray));
         long t0;
-        int k = 10000;
+        int k = 1000;
 
-        LinkedList list1 = new LinkedList();
+
         for (int n : nArray) {
-            int c = 0;
             long min = Long.MAX_VALUE;
-            for (int i = 0; i < n; i++) {
-                list1.addNode(c);
-                c++;
-            }
+
             for (int j = 0; j < k; j++) {
+                LinkedList list1 = new LinkedList();
+                LinkedList appended = new LinkedList();
+                LinkedList empty = new LinkedList();
+                for (int i = 0; i < n; i++) {
+                    list1.addNode(i);
+                }
+                for (int i = 0; i < 1000; i++) {
+                    appended.addNode(i);
+                }
                 long begin = System.nanoTime();
-                list1.getValue(n-2);
+                empty.addList(list1,appended);
                 long end = System.nanoTime();
                 t0 = (end - begin);
                 if (t0 < min) {
