ae17df12173df7bee3c37a8bd94f44bef3fdaa9d
999SH <94057132+999SH@users.noreply.github.com>
2022-09-27 20:26:53 +0200

    Lab 6 idek

--- src/Lab6/Main.java
+++ src/Lab6/Main.java
@@ -1,14 +1,36 @@
 package Lab6;
 
+import java.util.Random;
+
 public class Main {
     public static void main (String [] args){
-        BinaryTree Tree = new BinaryTree();
-        Tree.addLeaf(5,10);
-        Tree.addLeaf(2,9);
-        Tree.addLeaf(3,8);
-        Tree.addLeaf(8,7);
-        Tree.addLeaf(9,9);
-        Tree.addLeaf(1,7);
-        Tree.print();
+        Random generator = new Random();
+        int[] nArray = new int[7];
+        int ok = 1000;
+        for (int l = 0; l < nArray.length; l++) {
+            nArray[l] = ok;
+            ok = ok * 2;
+        }
+        System.out.println(java.util.Arrays.toString(nArray));
+
+        long t0;
+        int k = 10000;
+        for (int n : nArray) {
+            int[] sortedArray = new int[n];
+            for (int i = 0; i < n; i++) {
+                sortedArray[i] = i;
+            }
+            long min = Long.MAX_VALUE;
+            for (int j = 0; j < k; j++) {
+                long begin = System.nanoTime();
+                // code
+                long end = System.nanoTime();
+                t0 = (end - begin);
+                if (t0 < min) {
+                    min = t0;
+                }
+            }
+            System.out.println("Tree building " + (min));
+        }
     }
 }
